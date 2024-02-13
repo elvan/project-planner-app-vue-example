bc3a0d8b47474cde9128ea8ac4632f134f82e308
Elvan Hidayat <elvanhd@gmail.com>
2024-02-13 20:49:43 +0700

    Setup project configuration and structure
    - Add .browserslistrc to define browser support
    - Introduce .editorconfig for consistent coding styles
    - Update .gitignore to exclude unnecessary files and dirs
    - Configure Babel in babel.config.js for JavaScript transpilation
    - Create jsconfig.json for improved editor code intelligence
    - Initialize package.json with project dependencies
    - Add favicon and index.html in public directory
    - Setup initial Vue application structure in src folder
    - Implement basic routing with vue-router in src/router/index.js
    - Develop Home view component in src/views/Home.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+body {
+  background: #f2f2f2;
+}
+#app {
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
