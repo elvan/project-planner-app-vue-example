44b82eb71e3b7d3d31cb501a3b55690b720c9326
Elvan Hidayat <elvanhd@gmail.com>
2024-02-13 22:52:40 +0700

    Add AddProject view and route
    - Introduce AddProject view for new project submissions
    - Add route for AddProject in router configuration
    - Implement form in AddProject.vue with title and details fields
    - Style AddProject form for visual consistency and usability
    - Use v-model for form inputs to bind data properties
    - Handle form submission with handleSubmit method placeholder

---
+++ src/views/AddProject.vue
@@ -0,0 +1,66 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Title</label>
+    <input type="text" v-model="title" required />
+    <label>Details</label>
+    <textarea v-model="details" required></textarea>
+    <button>Add Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      details: '',
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.title, this.details);
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  padding: 20px;
+  border-radius: 10px;
+  background: white;
+}
+label {
+  display: block;
+  margin: 20px 0 10px 0;
+  color: #bbb;
+  font-weight: bold;
+  font-size: 14px;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+}
+input {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 10px;
+  border: 0;
+  border-bottom: 1px solid #ddd;
+}
+textarea {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100px;
+  padding: 10px;
+  border: 1px solid #ddd;
+}
+form button {
+  display: block;
+  margin: 20px auto 0;
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  color: white;
+  font-size: 16px;
+  background: #00ce89;
+}
+</style>
