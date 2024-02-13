d0a9a6a18277f5d15a5291fa4ca55744c49821e8
Elvan Hidayat <elvanhd@gmail.com>
2024-02-13 23:33:35 +0700

    Implement dynamic project filtering in Home view
    - Update Home.vue to display projects based on filter state
    - Introduce computed property filteredProjects for filtering logic
    - Handle project filtering for 'completed' and 'ongoing' states
    - Ensure all projects are displayed when 'all' filter is selected
    - Modify project iteration to use filteredProjects computed property

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <FilterNav :current="current" @filterChange="current = $event" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
@@ -39,7 +39,17 @@ export default {
         return project.id === id;
       });
       p.complete = !p.complete;
-      // console.log(p)
+    },
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter((project) => project.complete);
+      }
+      if (this.current === 'ongoing') {
+        return this.projects.filter((project) => !project.complete);
+      }
+      return this.projects;
     },
   },
 };
