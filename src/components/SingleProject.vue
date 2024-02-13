b24cc414cfe0bb2cbef8d7b2681246a473f2e135
Elvan Hidayat <elvanhd@gmail.com>
2024-02-13 21:07:44 +0700

    Add SingleProject component for project details
    - Introduce SingleProject component for individual projects
    - Implement toggle for project details in SingleProject
    - Use SingleProject in Home.vue to display projects
    - Style SingleProject with scoped CSS for better presentation
    - Enable project title click to toggle visibility of details

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+    </div>
+    <div v-if="showDetails" class="details">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+  data() {
+    return {
+      showDetails: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.project {
+  margin: 20px auto;
+  padding: 10px 20px;
+  border-left: 4px solid #e90074;
+  border-radius: 4px;
+  background: white;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+}
+h3 {
+  cursor: pointer;
+}
+</style>
