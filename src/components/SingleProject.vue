0d5bced124e61777734f61c97c0c3edd48041959
Elvan Hidayat <elvanhd@gmail.com>
2024-02-13 22:46:04 +0700

    Add toggle completion feature for projects
    - Implement visual indication for completed projects
    - Add `toggleComplete` method in SingleProject component
    - Use PATCH request to update project completion status
    - Emit `complete` event to notify Home view of status change
    - Update project completion visually in Home view on toggle
    - Style completed projects for clear visual differentiation

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
       <div class="icons">
         <span @click="deleteProject" class="material-icons">delete</span>
         <span class="material-icons">edit</span>
-        <span class="material-icons tick">done</span>
+        <span @click="toggleComplete" class="material-icons tick">done</span>
       </div>
     </div>
     <div v-if="showDetails" class="details">
@@ -27,6 +27,17 @@ export default {
     deleteProject() {
       fetch(this.uri, { method: 'DELETE' })
         .then(() => this.$emit('delete', this.project.id))
+        .catch((err) => console.log(err));
+    },
+    toggleComplete() {
+      fetch(this.uri, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ complete: !this.project.complete }),
+      })
+        .then(() => {
+          this.$emit('complete', this.project.id);
+        })
         .catch((err) => console.log(err));
     },
   },
@@ -58,5 +69,12 @@ h3 {
 }
 .material-icons:hover {
   color: #777;
+}
+/* completed projects */
+.project.complete {
+  border-left: 4px solid #00ce89;
+}
+.project.complete .tick {
+  color: #00ce89;
 }
 </style>
